545ad33d68ab88204bf36422c09594e8148ba540
meetahmed-a2m <devineprogrammer91@gmail.com>
2026-03-08 14:48:29 +0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+// let percentageCalc=()=>
+//     {     
+// let totalMarks = document.getElementById("tm").value
+// let obtMArks =document.getElementById("obt").value
+// let result = (obtMArks/totalMarks) * 100;
+// document.getElementById("rslt").value= result.toFixed(2);
+// if(totalMarks=="" || obtMArks=="")
+//     { 
+//     document.getElementById("rslt").value= "Please enter value";
+//     }}
+//  let formRest=()=>
+//     {
+//       document.getElementById("re-set").reset();
+//     }
 
-
-let percentageCalc=()=>
+let ZakatCalc=()=>
     {     
-let totalMarks = document.getElementById("tm").value
-let obtMArks =document.getElementById("obt").value
-let result = (obtMArks/totalMarks) * 100;
-document.getElementById("rslt").value= result.toFixed(2);
-if(totalMarks=="" || obtMArks=="")
+let TotalWealth = document.getElementById("tw").value
+let ZakatPercent =  (TotalWealth / 100) * 0.025;
+document.getElementById("rslt").value= ZakatPercent;
+
+
+if(TotalWealth=="")
     { 
     document.getElementById("rslt").value= "Please enter value";
     }}
@@ -14,3 +28,32 @@ if(totalMarks=="" || obtMArks=="")
     {
       document.getElementById("re-set").reset();
     }
+
+
+
+let BMICalc=()=>
+    {     
+let weight = document.getElementById("w8").value
+let height =document.getElementById("h8").value
+let BMIresult = weight / (height * height);
+document.getElementById("rslt2").value= BMIresult;
+
+if(weight=="" || height=="")
+    { 
+    document.getElementById("rslt2").value= "Please enter some value";
+    }}
+
+    // let formRest2=()=>
+    // {
+    //   document.getElementById("re-set").reset();
+    // }
+
+
+
+
+    // BMI Calculator
+// let weight = Number(prompt("Please Enter Your Weight"));
+// let height = Number(prompt("Please Enter Your Height"));
+// let BMIresult = weight / (height * height);
+// document.writeln("Your Body Mass Index is "+BMIresult.toFixed(2));
+
